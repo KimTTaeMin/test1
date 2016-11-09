e2dc61e76d483fb51ee359ab76b71c075f090ea2
KimTTaeMin <taemin9127@naver.com>
2016-11-09 16:25:05 +0900

    test1
    test1

---
+++ pyserial.py
@@ -0,0 +1,32 @@
+Python 2.7.10 (default, May 23 2015, 09:44:00) [MSC v.1500 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+ import serial
+ import time 
+
+ arduino = serial.Serial("/dev/ttyACM4", 9600) 
+ 
+ def onOffFunction(): 
+   command = raw_input("Type R/r/G/g: "); 
+ 	if command =="R":  
+ 		time.sleep(1)  
+ 		arduino.write('R')  
+ 		onOffFunction() 
+ 	elif command =="r": 
+ 		time.sleep(1)  
+ 		arduino.write('r') 
+ 		onOffFunction() 
+ 	elif command =="G":
+		time.sleep(1)
+		arduino.write('G')  
+ 		onOffFunction()
+ 	elif command =="g":
+		time.sleep(1)
+		arduino.write('g')  
+ 		onOffFunction()
+ 	else:  
+ 		time.sleep(1)  
+ 		arduino.close() 
+ 
+ time.sleep(2) 
+ 
+ onOffFunction() 
